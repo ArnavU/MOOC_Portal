534aeb61f1e3cc70331a3dc228496452e0d58ff7
ArnavU <arnavumarkar.intern@peopleprudent.co.in>
2025-04-28 16:59:59 +0530

    resolve mail sender not working

--- server/controllers/ContactUs.js
+++ server/controllers/ContactUs.js
@@ -1,8 +1,9 @@
-const mailSender = require("../utils/mailSender");
+const {mailSender} = require("../utils/mailSender");
 
 exports.contactUs = async (req, res) => {
   const { firstName, lastName, email, message, phoneNo } = req.body;
   if (!firstName || !email || !message) {
+    console.log("All fields are necessary in contact us form")
     return res.status(403).send({
       success: false,
       message: "All Fields are required",
@@ -29,12 +30,14 @@ exports.contactUs = async (req, res) => {
         message: "Your message has been sent successfully",
       });
     } else {
+      console.log("Something went wrong - mail not sent")
       return res.status(403).send({
         success: false,
         message: "Something went wrong",
       });
     }
   } catch (error) {
+    console.log("Some error occured in contact us form: ", error.message)
     return res.status(403).send({
       success: false,
       message: "Something went wrong",
--- server/controllers/Payments.js
+++ server/controllers/Payments.js
@@ -1,7 +1,7 @@
 const {instance} = require("../config/razorpay");
 const Course = require("../models/Course");
 const User = require("../models/User");
-const mailSender = require("../utils/mailSender");
+const {mailSender} = require("../utils/mailSender");
 const {courseEnrollmentEmail} = require("../mail/templates/courseEnrollmentEmail");
 const {paymentSuccess} = require("../mail/templates/paymentSuccess");
 const { default: mongoose } = require("mongoose");
